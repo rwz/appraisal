3e974e60eb0360321c95e33dcea733f087f7342e
Pavel Pravosud <pavel@pravosud.com>
2013-07-17 00:14:12 +0700

    Command fix

--- lib/appraisal/command.rb
+++ lib/appraisal/command.rb
@@ -4,7 +4,7 @@ module Appraisal
     BUNDLER_ENV_VARS = %w(RUBYOPT BUNDLE_PATH BUNDLE_BIN_PATH BUNDLE_GEMFILE).freeze
 
     def self.from_args(gemfile)
-      command = ([$0] + ARGV.slice(1, ARGV.size)).join(' ')
+      command = [ $0, *ARGV.drop(1) ].join(' ')
       new(command, gemfile)
     end
 
